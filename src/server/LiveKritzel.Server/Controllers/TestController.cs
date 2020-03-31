0978b06e4dd9a0c3814b03fea4d1723ff41a0473
maSchoeller <git@marvinschoeller.de>
2020-03-31 21:50:07 +0200

    update

---
+++ src/server/LiveKritzel.Server/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveKritzel.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet()]
+        public string GetRunning()
+        {
+            return "Server is Running";
+        }
+    }
+}
